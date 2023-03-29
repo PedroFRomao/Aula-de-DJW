ad6642ad8e2c9e4ff891c00d6a3634e98ebcb106
Pedro Romão <pedroferraz.romao@gmail.com>
2023-03-28 22:08:33 -0300

    js do projeto

---
+++ Site da troca de imagens/script.js
@@ -0,0 +1,5 @@
+function trocarImagem() {
+    let imagem = document.getElementById("imagem");
+    let aleatorio = Math.trunc (Math.random()*10);
+    imagem.src = "img/imagem" + aleatorio + ".jpg";
+  }
